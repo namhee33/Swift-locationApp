04ba5b8bcc4a0a117ee851677441cdec8fd918a6
Susanna <nsh1026@uw.edu>
2016-01-18 16:21:22 -0800

    Initial commit

---
+++ LocationApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationApp
+//
+//  Created by Shuhan Ng on 1/18/16.
+//  Copyright © 2016 Shuhan Ng. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+
+class ViewController: UIViewController{
+    var location: CLLocationManager!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+
+
+}
+
